7df1f112b9636078a191a1011363d546af2397c0
fanyxx900 <fanyxmedia@gmail.com>
2023-09-16 13:53:00 +0300

    up

--- Indicators.py
+++ Indicators.py
@@ -1,4 +1,4 @@
-from AlgorithmImports import *
+﻿from AlgorithmImports import *
 
 # Indicatori pentru 4 ore
 def ema50_4h_btc(algo, symbol):
@@ -17,3 +17,19 @@ def ema50_15m_btc(algo, symbol):
 def ema50_15m_eth(algo, symbol):
     period = 50*15  # 50 bars of 15 minutes
     return algo.EMA(symbol, period, Resolution.Minute)
+
+# Calculul lumânărilor Heikin-Ashi
+def heikin_ashi(algo, symbol, resolution=Resolution.Minute):
+    history = algo.History(symbol, 2, resolution)
+    if not history.empty:
+        previous_bar = history.iloc[-2]
+        current_bar = history.iloc[-1]
+        
+        ha_open = (previous_bar['open'] + previous_bar['close']) / 2
+        ha_close = (current_bar['open'] + current_bar['close'] + current_bar['high'] + current_bar['low']) / 4
+        ha_high = max(current_bar['high'], ha_open, ha_close)
+        ha_low = min(current_bar['low'], ha_open, ha_close)
+        
+        return {'open': ha_open, 'close': ha_close, 'high': ha_high, 'low': ha_low}
+    else:
+        return None
